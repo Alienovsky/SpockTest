5bee7e9d9cacbb43be9a0d62f2090c1fbf4050f4
Kamil Trepczynski <kamil.trepczynski@rulefinancial.com>
2015-03-12 16:52:13 +0100

    added Mirror Calc groovy class in java class added clear calctest with comments

---
+++ src/test/groovy/firststeps/calcTestClear.groovy
@@ -0,0 +1,19 @@
+package firststeps
+
+class CalcTestClear {
+    //####################### Ways of writing
+
+    //####################### Param
+
+    //####################### Some exceptions?
+
+    //do you remember hack with try catch?!
+    /*
+        try{
+            doSth();
+            assert.Fail();
+        }catch(Exception e){
+            assertThat(e.getMessage().contains("some error"));
+        }
+     */
+}
